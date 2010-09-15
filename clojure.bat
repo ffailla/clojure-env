96f73391998d334ee69206eda30a44d5d3a43c2c
Frank Failla <ffailla@gmail.com>
2010-09-15 13:54:21 -0400

    removed server from jvm args.  fixed windows batch script.  added jline to mvn as dependency

--- clojure.bat
+++ clojure.bat
@@ -7,15 +7,19 @@ for /R ./lib %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!"
 set CLASSPATH=%CLASSPATH%;src;test;config;data
 set CLASSPATH="
-for /R %USERPROFILE%/.clojure/lib %%a in (*.jar) do (
+for /R "%USERPROFILE%/.clojure/lib" %%a in (*.jar) do (
    set CLASSPATH=!CLASSPATH!;%%a
 )
 set CLASSPATH=!CLASSPATH!"
 echo CLASSPATH=%CLASSPATH%
 
 rem check if the first arg is a file, if so use clj-script
-if exist %1 
-  java -server -Xmx1G -cp $CLASSPATH jline.ConsoleRunner clojure.main %*
-else
-  java -server -Xmx1G -cp $CLASSPATH jline.ConsoleRunner clojure.main -r %*
+if "%1"=="" goto :noscript
 
+if not exist %1 goto :noscript 
+  rem java -server -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main %*
+  java -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main %*
+
+:noscript
+  rem java -server -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main -r %*
+  java -Xmx1G -cp %CLASSPATH% jline.ConsoleRunner clojure.main -r %*
